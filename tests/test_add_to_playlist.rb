012ded2a747e739ddeaa4f04d25b84c3858915e6
Jabari Brown <161641923+barib120@users.noreply.github.com>
2024-03-22 06:34:23 +0000

    I altered the test_add_song method

--- tests/test_add_to_playlist.rb
+++ tests/test_add_to_playlist.rb
@@ -2,14 +2,37 @@ require 'minitest/autorun'
 require './add_to_playlist'
 require 'stringio'
 class Test_add_song < Minitest::Test
-  def test_user_input
-    simulated_user_input = StringIO.new("your input here\n")
-    $stdin = simulated_user_input
+  def test_add_song
+  input = "Eternal Sunshine\n"
+  second_input = "Ariana Grande\n"
+  expected_output = "Song added\n"
 
-    output = add_song
+   # Redirect stdin to simulate user input
+   $stdin = StringIO.new(input)
 
-    $stdin = STDIN
-    assert_equal "expected output", output
+   # Redirect stdout to capture output
+   $stdout = StringIO.new
+
+   # Call the method that takes user input
+   result = add_song
+
+   # Reset stdin and get the captured output
+   $stdin = STDIN
+   actual_output = $stdout.string
+
+   # Assert that the output matches the expected output
+   assert_equal(expected_output, actual_output)
+
+  #  simulated_user_input = StringIO.new("your input here\n")
+  #  $stdin = simulated_user_input
+
+  #  output = add_song
+
+ #   $stdin = STDIN
+ #   assert_equal "expected output", output
+
+
     
   end
+
 end
