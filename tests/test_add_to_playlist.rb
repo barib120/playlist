5595c04946a97ed5d2d10a7b2233e1a979b85825
Jabari Brown <161641923+barib120@users.noreply.github.com>
2024-03-22 16:10:35 +0000

    Struggling to define the "add_song"

--- tests/test_add_to_playlist.rb
+++ tests/test_add_to_playlist.rb
@@ -1,19 +1,25 @@
 require 'minitest/autorun'
 require_relative '../add_to_playlist'
 require 'stringio'
+
 class Test_add_song < Minitest::Test
   def test_add_song
- # input = "Eternal Sunshine\nAriana Grande\n"
-  
- $stdin = StringIO.new("Eternal Sunshine\nAriana Grande\n")
- expected_output = "Song added\n"
-  assert_equal(expected_output, add_song)
+  input = "Eternal Sunshine\nAriana Grande\n"
+  expected_output = "Song added\n"
+    # $stdin = StringIO.new("Eternal Sunshine\nAriana Grande\n")
+ 
+      # assert_equal(expected_output, add_song)
  
   
+  $stdin = StringIO.new(input)
+  $stdout = StringIO.new
+
+  result = add_song
+
   $stdin = STDIN
-  
+  actual_output = $stdout.string
 
-
+  assert_equal(expected_output, actual_output)
     
   end
 
