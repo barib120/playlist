b4944cd667c5fcd5a6cc508b2ab8594cf2146444
Jabari Brown <161641923+barib120@users.noreply.github.com>
2024-03-23 04:09:40 +0000

    trying some new code

--- add_to_playlist.rb
+++ add_to_playlist.rb
@@ -1,6 +1,6 @@
-require_relative  "songs"
 
-pp "Hi, welcome to your playlist"
+=begin
+
 
 class Playlist
   def initialize
@@ -72,3 +72,65 @@ class Playlist
 end
 
 #new_instance = Playlist.new
+
+=end
+
+class Playlist
+  attr_accessor :songs
+
+  def initialize
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    puts "#{song} added to your playlist."
+  end
+
+  def display_playlist
+    puts "Your playlist:"
+    @songs.each_with_index do |song, index|
+      puts "#{index + 1}. #{song}"
+    end
+  end
+end
+
+playlist = Playlist.new
+
+loop do
+  puts "\nOptions:"
+  puts "1. See your playlist"
+  puts "2. Add a song to your playlist"
+  puts "3. Exit"
+  print "Enter your choice: "
+  choice = gets.chomp.to_i
+
+  case choice
+  when 1
+    loop do
+      puts "\nPlaylist Options:"
+      puts "1. View current songs"
+      puts "2. Back to main menu"
+      print "Enter your choice: "
+      sub_choice = gets.chomp.to_i
+
+      case sub_choice
+      when 1
+        playlist.display_playlist
+      when 2
+        break
+      else
+        puts "Invalid choice. Please try again."
+      end
+    end
+  when 2
+    print "Enter the name of the song to add: "
+    song = gets.chomp
+    playlist.add_song(song)
+  when 3
+    puts "Goodbye!"
+    break
+  else
+    puts "Invalid choice. Please try again."
+  end
+end
