0e1651ad995ca61242cd9c1f766355451990bfa7
Jabari Brown <161641923+barib120@users.noreply.github.com>
2024-03-23 04:21:07 +0000

    created a mini test with new code

--- tests/test_add_to_playlist.rb
+++ tests/test_add_to_playlist.rb
@@ -1,27 +1,18 @@
 #require 'minitest/autorun'
 require 'test/unit'
 require_relative '../add_to_playlist'
-require 'stringio'
 
-class Test_add_song < Test::Unit::TestCase
-  def test_add_song
-  input = "Eternal Sunshine\nAriana Grande\n"
-  expected_output = "Song added\n"
-    # $stdin = StringIO.new("Eternal Sunshine\nAriana Grande\n")
- 
-      # assert_equal(expected_output, add_song)
- 
-  
-  $stdin = StringIO.new(input)
-  $stdout = StringIO.new
 
-  result = add_song
-
-  $stdin = STDIN
-  actual_output = $stdout.string
-
-  assert_equal(expected_output, actual_output)
-    
+class TestPlaylist < Test::Unit::TestCase
+  def setup
+    @playlist = Playlist.new
   end
 
+  def test_add_song
+    @playlist.add_song("Song 1")
+    assert_equal(["Song 1"], @playlist.songs)
+
+    @playlist.add_song("Song 2")
+    assert_equal(["Song 1", "Song 2"], @playlist.songs)
+  end
 end
